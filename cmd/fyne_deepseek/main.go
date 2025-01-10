48b31560d265ce82768538d6da12980d0f69da6f
tming <tm790107@163.com>
2025-01-10 17:59:27 +0800

    feat:add one example

---
+++ cmd/fyne_deepseek/main.go
@@ -0,0 +1,344 @@
+// Package main provides various examples of Fyne API capabilities.
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/cmd/fyne_deepseek/data"
+	"fyne.io/fyne/v2/cmd/fyne_settings/settings"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+
+var topWindow fyne.Window
+
+func main() {
+	a := app.NewWithID("fyne.deepseek.tm")
+	a.SetIcon(data.FyneLogo)
+	makeTray(a)
+	// logLifecycle(a)
+	w := a.NewWindow("fyne.deepseek Demo")
+	topWindow = w
+
+	w.SetMainMenu(makeMenu(a, w))
+	w.SetMaster()
+
+	promptLabel := widget.NewLabel("prompt")
+	promptText := makeText("input your prompt here...")
+
+	tokenLabel := widget.NewLabel("token")
+	tokenText := makeText("input your deepseek-token here...")
+
+	contentText := makeText("here is the response")
+
+	button := makeButton("生成", func() {
+		entryPrompt, ok := promptText.(*widget.Entry)
+		if !ok {
+			log.Panicln("failed to convert fyne.CanvasObject to *widget.Entry")
+		}
+		entryToken, ok := tokenText.(*widget.Entry)
+		if !ok {
+			log.Panicln("failed to convert fyne.CanvasObject to *widget.Entry")
+		}
+		log.Printf("ready query deepseek with prompt(%s) token(%s) now...\n", entryPrompt.Text, entryToken.Text)
+
+		response := queryDeepSeek(entryPrompt.Text, entryToken.Text)
+
+		entryContent, ok := contentText.(*widget.Entry)
+		if !ok {
+			log.Panicln("failed to convert fyne.CanvasObject to *widget.Entry")
+		}
+		entryContent.SetText(response)
+	})
+
+	// content := container.NewStack()
+	// content.Objects = []fyne.CanvasObject{makeText("")}
+
+	win := container.NewBorder(
+		container.NewVBox(promptLabel, promptText, widget.NewSeparator(), tokenLabel, tokenText, button),
+		nil, nil, nil, contentText)
+
+	w.SetContent(win)
+	w.Resize(fyne.NewSize(640, 460))
+	w.ShowAndRun()
+}
+
+// func logLifecycle(a fyne.App) {
+// 	a.Lifecycle().SetOnStarted(func() {
+// 		log.Println("Lifecycle: Started")
+// 	})
+// 	a.Lifecycle().SetOnStopped(func() {
+// 		log.Println("Lifecycle: Stopped")
+// 	})
+// 	a.Lifecycle().SetOnEnteredForeground(func() {
+// 		log.Println("Lifecycle: Entered Foreground")
+// 	})
+// 	a.Lifecycle().SetOnExitedForeground(func() {
+// 		log.Println("Lifecycle: Exited Foreground")
+// 	})
+// }
+
+func makeMenu(a fyne.App, w fyne.Window) *fyne.MainMenu {
+	newItem := fyne.NewMenuItem("New", nil)
+	checkedItem := fyne.NewMenuItem("Checked", nil)
+	checkedItem.Checked = true
+	disabledItem := fyne.NewMenuItem("Disabled", nil)
+	disabledItem.Disabled = true
+	otherItem := fyne.NewMenuItem("Other", nil)
+	mailItem := fyne.NewMenuItem("Mail", func() { fmt.Println("Menu New->Other->Mail") })
+	mailItem.Icon = theme.MailComposeIcon()
+	otherItem.ChildMenu = fyne.NewMenu("",
+		fyne.NewMenuItem("Project", func() { fmt.Println("Menu New->Other->Project") }),
+		mailItem,
+	)
+	fileItem := fyne.NewMenuItem("File", func() { fmt.Println("Menu New->File") })
+	fileItem.Icon = theme.FileIcon()
+	dirItem := fyne.NewMenuItem("Directory", func() { fmt.Println("Menu New->Directory") })
+	dirItem.Icon = theme.FolderIcon()
+	newItem.ChildMenu = fyne.NewMenu("",
+		fileItem,
+		dirItem,
+		otherItem,
+	)
+
+	openSettings := func() {
+		w := a.NewWindow("Fyne Settings")
+		w.SetContent(settings.NewSettings().LoadAppearanceScreen(w))
+		w.Resize(fyne.NewSize(440, 520))
+		w.Show()
+	}
+	showAbout := func() {
+		w := a.NewWindow("About")
+		w.SetContent(widget.NewLabel("About Fyne Demo app..."))
+		w.Show()
+	}
+	aboutItem := fyne.NewMenuItem("About", showAbout)
+	settingsItem := fyne.NewMenuItem("Settings", openSettings)
+	settingsShortcut := &desktop.CustomShortcut{KeyName: fyne.KeyComma, Modifier: fyne.KeyModifierShortcutDefault}
+	settingsItem.Shortcut = settingsShortcut
+	w.Canvas().AddShortcut(settingsShortcut, func(shortcut fyne.Shortcut) {
+		openSettings()
+	})
+
+	cutShortcut := &fyne.ShortcutCut{Clipboard: w.Clipboard()}
+	cutItem := fyne.NewMenuItem("Cut", func() {
+		shortcutFocused(cutShortcut, w)
+	})
+	cutItem.Shortcut = cutShortcut
+	copyShortcut := &fyne.ShortcutCopy{Clipboard: w.Clipboard()}
+	copyItem := fyne.NewMenuItem("Copy", func() {
+		shortcutFocused(copyShortcut, w)
+	})
+	copyItem.Shortcut = copyShortcut
+	pasteShortcut := &fyne.ShortcutPaste{Clipboard: w.Clipboard()}
+	pasteItem := fyne.NewMenuItem("Paste", func() {
+		shortcutFocused(pasteShortcut, w)
+	})
+	pasteItem.Shortcut = pasteShortcut
+	performFind := func() { fmt.Println("Menu Find") }
+	findItem := fyne.NewMenuItem("Find", performFind)
+	findItem.Shortcut = &desktop.CustomShortcut{KeyName: fyne.KeyF, Modifier: fyne.KeyModifierShortcutDefault | fyne.KeyModifierAlt | fyne.KeyModifierShift | fyne.KeyModifierControl | fyne.KeyModifierSuper}
+	w.Canvas().AddShortcut(findItem.Shortcut, func(shortcut fyne.Shortcut) {
+		performFind()
+	})
+
+	helpMenu := fyne.NewMenu("Help",
+		fyne.NewMenuItem("Documentation", func() {
+			u, _ := url.Parse("https://developer.fyne.io")
+			_ = a.OpenURL(u)
+		}),
+		fyne.NewMenuItem("Support", func() {
+			u, _ := url.Parse("https://fyne.io/support/")
+			_ = a.OpenURL(u)
+		}),
+		fyne.NewMenuItemSeparator(),
+		fyne.NewMenuItem("Sponsor", func() {
+			u, _ := url.Parse("https://fyne.io/sponsor/")
+			_ = a.OpenURL(u)
+		}))
+
+	// a quit item will be appended to our first (File) menu
+	file := fyne.NewMenu("File", newItem, checkedItem, disabledItem)
+	device := fyne.CurrentDevice()
+	if !device.IsMobile() && !device.IsBrowser() {
+		file.Items = append(file.Items, fyne.NewMenuItemSeparator(), settingsItem)
+	}
+	file.Items = append(file.Items, aboutItem)
+	main := fyne.NewMainMenu(
+		file,
+		fyne.NewMenu("Edit", cutItem, copyItem, pasteItem, fyne.NewMenuItemSeparator(), findItem),
+		helpMenu,
+	)
+	checkedItem.Action = func() {
+		checkedItem.Checked = !checkedItem.Checked
+		main.Refresh()
+	}
+	return main
+}
+
+func makeTray(a fyne.App) {
+	if desk, ok := a.(desktop.App); ok {
+		h := fyne.NewMenuItem("Hello", func() {})
+		h.Icon = theme.HomeIcon()
+		menu := fyne.NewMenu("Hello World", h)
+		h.Action = func() {
+			log.Println("System tray menu tapped")
+			h.Label = "Welcome"
+			menu.Refresh()
+		}
+		desk.SetSystemTrayMenu(menu)
+	}
+}
+
+func shortcutFocused(s fyne.Shortcut, w fyne.Window) {
+	switch sh := s.(type) {
+	case *fyne.ShortcutCopy:
+		sh.Clipboard = w.Clipboard()
+	case *fyne.ShortcutCut:
+		sh.Clipboard = w.Clipboard()
+	case *fyne.ShortcutPaste:
+		sh.Clipboard = w.Clipboard()
+	}
+	if focused, ok := w.Canvas().Focused().(fyne.Shortcutable); ok {
+		focused.TypedShortcut(s)
+	}
+}
+
+func makeText(placeHolder string) fyne.CanvasObject {
+	entryLoremIpsum := widget.NewMultiLineEntry()
+	entryLoremIpsum.SetPlaceHolder(placeHolder)
+	entryLoremIpsum.Wrapping = fyne.TextWrapWord
+
+	return entryLoremIpsum
+}
+
+func makeInput() fyne.CanvasObject {
+	selectEntry := widget.NewSelectEntry([]string{})
+	selectEntry.PlaceHolder = "Type or select"
+
+	return selectEntry
+}
+
+func makeButton(text string, f func()) fyne.CanvasObject {
+	return &widget.Button{
+		Text:       text,
+		Importance: widget.HighImportance,
+		OnTapped:   f,
+	}
+}
+
+// ----------------------------------deep seek-------------------
+func queryDeepSeek(content, token string) string {
+
+	url := "https://api.deepseek.com/chat/completions"
+	method := "POST"
+
+	payload := strings.NewReader(fmt.Sprintf(`{
+  "messages": [
+    {
+      "content": "你是一个小说作家，擅长悬疑恐怖类型的写作",
+      "role": "system"
+    },
+	{
+      "content": "%s",
+      "role": "user"
+    }
+  ],
+  "id": "28cc3ae1-e96b-48fb-8cff-286209960234",
+  "model": "deepseek-chat",
+  "frequency_penalty": 0,
+  "max_tokens": 2048,
+  "presence_penalty": 0,
+  "response_format": {
+    "type": "text"
+  },
+  "stop": null,
+  "stream": false,
+  "stream_options": null,
+  "temperature": 1,
+  "top_p": 1,
+  "tools": null,
+  "tool_choice": "none",
+  "logprobs": false,
+  "top_logprobs": null
+}`, content))
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	var chatResponse ChatCompletion
+
+	// Unmarshal the JSON string into the Example struct
+	err = json.Unmarshal(body, &chatResponse)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		return ""
+	}
+
+	if len(chatResponse.Choices) > 0 {
+		return chatResponse.Choices[0].Message.Content
+	}
+
+	return ""
+}
+
+type ChatCompletion struct {
+	ID                string   `json:"id"`
+	Object            string   `json:"object"`
+	Created           int64    `json:"created"`
+	Model             string   `json:"model"`
+	Choices           []Choice `json:"choices"`
+	Usage             Usage    `json:"usage"`
+	SystemFingerprint string   `json:"system_fingerprint"`
+}
+
+type Choice struct {
+	Index        int     `json:"index"`
+	Message      Message `json:"message"`
+	Logprobs     *string `json:"logprobs"` // Assuming logprobs can be null
+	FinishReason string  `json:"finish_reason"`
+}
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type Usage struct {
+	PromptTokens          int `json:"prompt_tokens"`
+	CompletionTokens      int `json:"completion_tokens"`
+	TotalTokens           int `json:"total_tokens"`
+	PromptCacheHitTokens  int `json:"prompt_cache_hit_tokens"`
+	PromptCacheMissTokens int `json:"prompt_cache_miss_tokens"`
+}
